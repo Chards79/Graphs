0c9959020553cb6c04bcf2e4c16cf6aacfe810b5
Eric Richards <esholundr@yahoo.com>
2020-08-11 20:32:33 -0600

    ancestor passing all tests

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,30 +1,40 @@
 from stack import Stack
 
 
-# def __init__(self):
-#     self.vertices = {}
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        self.vertices[vertex_id] = set()
+
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("Vertex does not exist")
+
+    def get_neighbors(self, vertex_id):
+        return self.vertices[vertex_id]
 
 
-# def add_vertex(self, vertex_id):
-#     self.vertices[vertex_id] = set()
+def earliest_ancestor(ancestors, starting_node):
+    graph = Graph()
 
+    for pair in ancestors:
+        graph.add_vertex(pair[0])
+        graph.add_vertex(pair[1])
 
-# def add_edge(self, v1, v2):
-#     if v1 in self.vertices and v2 in self.vertices:
-#         self.vertices[v1].add(v2)
-#     else:
-#         raise IndexError("Vertex does not exist")
+    for pair in ancestors:
+        graph.add_edge(pair[1], pair[0])
 
-
-def get_neighbors(self, vertex_id):
-    return self.vertices[vertex_id]
-
-
-def earliest_ancestor(self, starting_node, ending_node):
     s = Stack()
     s.push([starting_node])
 
     visited = set()
+
+    path_list = list()
+    path_len_list = list()
 
     while s.size() > 0:
         path = s.pop()
@@ -32,17 +42,26 @@ def earliest_ancestor(self, starting_node, ending_node):
         last_vertex = path[-1]
 
         if last_vertex not in visited:
-            # logic to choose lowest numeric ID if more than one earliest ancestor
-            if last_vertex == ending_node:
-                return path
+
+            path_list.append(path)
+            path_len_list.append(len(path))
 
             visited.add(last_vertex)
 
-            for next_vertex in self.get_neighbors(last_vertex):
+            for next_vertex in graph.get_neighbors(last_vertex):
                 path_copy = list(path)
 
                 path_copy.append(next_vertex)
 
                 s.push(path_copy)
+    print(path_list)
+    max_len = max(path_len_list)
+    tied_earliest = list()
 
-    return None
+    if max_len == 1:
+        return -1
+    else:
+        for path in path_list:
+            if len(path) == max_len:
+                tied_earliest.append(path[-1])
+        return min(tied_earliest)
